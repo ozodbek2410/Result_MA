b0b08d7480d07f70730717745bc5fdb6de69d8c0
Yuxanno <ravshanovyuxanno1105@mail.ru>
2026-02-03 23:30:52 +0500

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,24 +1,22 @@
 module.exports = {
-  apps: [
-    {
-      name: 'mathacademy-server',
-      script: './server/dist/index.js',
-      instances: 1,
-      exec_mode: 'cluster',
-      env: {
-        NODE_ENV: 'production',
-        PORT: 9999,
-        MONGODB_URI: 'mongodb+srv://resultma2_db_user:10qyG6hxMdd8H2XW@cluster0.tlffh49.mongodb.net/test',
-        JWT_SECRET: 'education_system_secret_key_2026'
-      },
-      error_file: './logs/server-error.log',
-      out_file: './logs/server-out.log',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-      merge_logs: true,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      exp_backoff_restart_delay: 100
-    }
-  ]
+  apps: [{
+    name: 'mathacademy-server',
+    script: './server/dist/index.js',
+    instances: 1,
+    exec_mode: 'cluster',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 9999,
+      MONGODB_URI: 'mongodb+srv://resultma2_db_user:10qyG6hxMdd8H2XW@cluster0.tlffh49.mongodb.net/test',
+      JWT_SECRET: 'education_system_secret_key_2026'
+    },
+    error_file: './logs/server-error.log',
+    out_file: './logs/server-out.log',
+    log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+    merge_logs: true,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    exp_backoff_restart_delay: 100
+  }]
 };
